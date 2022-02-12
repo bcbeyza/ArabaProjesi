fe4ee2d8c3437aa9a0c6834bbdc93520127e61f2
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2022-02-13 00:54:59 +0300

    13.02.2022

---
+++ SQLQuery3.sql
@@ -0,0 +1,11 @@
+﻿  create table CarImages(
+    Id int not null,
+    CarId int not null,
+    ImagePath nvarchar(100)null,
+    Date DateTime null,
+    primary key(Id),
+    CONSTRAINT FK_CarImages_CarId FOREIGN KEY(CarId) REFERENCES Cars(Id)
+
+
+
+    )
